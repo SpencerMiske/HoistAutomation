a8767d8d51eac7f0de8934da479348e44ca28f17
Spencer <95696502+SpencerMiske@users.noreply.github.com>
2024-06-25 17:32:16 +0100

    Update Hoist.py

--- Hoist.py
+++ Hoist.py
@@ -2,47 +2,67 @@
 
 import RPi.GPIO as io
 
+io.cleanup()
+
 class system:
+    visualList = []
     
-    def __init__(self, zones, initialZone, in1, in2, hoistSpeed):
+    def __init__(self, zones, initialZone, hoistSpeed):
         self.zones = zones
         self.speed = hoistSpeed
         self.currentZone = initialZone
         
+        for i in range(zones):
+            self.visualList.append('0')
+            
+        self.visualList[self.currentZone] = 'X'
+        
+        
+    def setMotor(self, in1, in2):
         io.setup(in1, io.OUT)
         io.setup(in2, io.OUT)
         
-        backward = io.PWM(in2, 100)
-        backward.start(0)
+        self.backward = io.PWM(in2, 100)
+        self.backward.start(0)
 
-        forward = io.PWM(in1, 100)
-        forward.start(0)
+        self.forward = io.PWM(in1, 100)
+        self.forward.start(0)
     
     def move(self, speed):
         if speed > 0:
-            forward.ChangeDutyCycle(speed)
-            backward.ChangeDutyCycle(0)
+            self.forward.ChangeDutyCycle(self.speed)
+            self.backward.ChangeDutyCycle(0)
+        elif speed == 0:
+            self.forward.ChangeDutyCycle(0)
+            self.backward.ChangeDutyCycle(0)
         else:
-            forward.ChangeDutyCycle(0)
-            backward.ChangeDutyCycle(speed)
+            self.forward.ChangeDutyCycle(0)
+            self.backward.ChangeDutyCycle(self.speed)
             
-    def stop():
-        forward.ChangeDutyCycle(0)
-        backward.ChangeDutyCycle(0)
+
         
     def moveTo(self, endZone):
-        if endZone > currentZone: direction = 1
-        else direction = -1
+        self.visualList[endZone] = 'I'
         
-        self.move(speed*direction)
+        if endZone > self.currentZone: direction = 1
+        else: direction = -1
         
-        while currentZone != endZone:
-            buttonState = io.input(buttonPin)
+        print('moving')
+        
+        self.move(self.speed*direction)
+       
+        while self.currentZone != endZone:
+            print(self.visualList)
+            check = input()
             
-            if buttonState = 0:
-                currentZone += direction
+            if check == ' ':
+                self.visualList[self.currentZone] = '0'
+                self.currentZone += direction
+                self.visualList[self.currentZone] = 'X'
                 
-        self.stop
+        print('stop moving')
+        self.move(0)
+
         print('Destination Reached')
 
             
