11a9a2d425cf33b228443bdd1e31c4b6ebd5e019
Spencer <95696502+SpencerMiske@users.noreply.github.com>
2024-06-25 20:04:28 +0100

    Update Hoist.py

--- Hoist.py
+++ Hoist.py
@@ -1,68 +1,55 @@
-##Hoist Class
-
 import RPi.GPIO as io
+from time import sleep
+import sys
+from Hoist import system
 
-io.cleanup()
+##Setup
+io.setmode(io.BOARD)
 
-class system:
-    visualList = []
+##Initilaize Variables
+buttonPin = 11
+ledPin = 13
+buttonState = 0
+
+tankNum = 5
+in1 = 16
+in2 = 18
+
+##GPIO Pin Setup
+zirc = system(tankNum, 2, 30)
+zirc.setMotor(in1, in2)
+
+io.setup(buttonPin, io.IN,pull_up_down=io.PUD_UP)
+io.setup(ledPin, io.OUT)
+io.output(ledPin, 1)
+
+#Wait for button press to start
+while True:
+    buttonCheck = io.input(buttonPin)
+    if buttonCheck == 0:
+        break
+    sleep(0.1)
+
+#Emergency exit button
+def button_callback(channel):
     
-    def __init__(self, zones, initialZone, hoistSpeed):
-        self.zones = zones
-        self.speed = hoistSpeed
-        self.currentZone = initialZone
-        
-        for i in range(zones):
-            self.visualList.append('0')
-            
-        self.visualList[self.currentZone] = 'X'
-        
-        
-    def setMotor(self, in1, in2):
-        io.setup(in1, io.OUT)
-        io.setup(in2, io.OUT)
-        
-        self.backward = io.PWM(in2, 100)
-        self.backward.start(0)
-
-        self.forward = io.PWM(in1, 100)
-        self.forward.start(0)
+    print('Exit button pressed')
+    io.output(ledPin,0)
+    io.cleanup()
+    sys.exit(0)
     
-    def move(self, speed):
-        if speed > 0:
-            self.forward.ChangeDutyCycle(self.speed)
-            self.backward.ChangeDutyCycle(0)
-        elif speed == 0:
-            self.forward.ChangeDutyCycle(0)
-            self.backward.ChangeDutyCycle(0)
-        else:
-            self.forward.ChangeDutyCycle(0)
-            self.backward.ChangeDutyCycle(self.speed)
-            
+io.add_event_detect(buttonPin, io.FALLING, callback=button_callback, bouncetime=200)
 
+try:
+    print('Start')
+    while True:
         
-    def moveTo(self, endZone):
-        self.visualList[endZone] = 'I'
+        command = int(input('Next tank ready: '))
+        if command > zirc.zones: print('tank not in range')
+        else: zirc.moveTo(command-1)
         
-        if endZone > self.currentZone: direction = 1
-        else: direction = -1
-        
-        print('moving')
-        
-        self.move(self.speed*direction)
-       
-        while self.currentZone != endZone:
-            print(self.visualList)
-            check = input()
-            
-            if check == ' ':
-                self.visualList[self.currentZone] = '0'
-                self.currentZone += direction
-                self.visualList[self.currentZone] = 'X'
-                
-        print('stop moving')
-        self.move(0)
+except KeyboardInterrupt:
+    print('inturrupted by user')
+finally:
+    io.cleanup()
 
-        print('Destination Reached')
-
-            
