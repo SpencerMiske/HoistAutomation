dee4d56f64da5fb8924a83e22eebbbe6768b5460
Spencer <95696502+SpencerMiske@users.noreply.github.com>
2024-07-22 18:27:10 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,20 +2,25 @@ import RPi.GPIO as io
 from time import sleep
 import sys
 import threading
-from Hoist import system
 from Job import job
+import serial
 
 from flask import Flask, request, jsonify
 
+ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
 ##Initilaize Variables
 BUTTONPIN = 11
 TANKNUM = 5
 IN1 = 16
 IN2 = 18
-SENSORPIN = 35
+SENSORPIN = 40
 STARTRACK = 0
 ENDRACK = TANKNUM-1
-RACKSPEED = 40
+RACKSPEED = 35
+
+ZONENUM = 6
+TANKLOC = [0,1000,2000,3000,4000,5000]
+occupiedTanks = [0,0,0,0,0,0]
 
 moveQueue = []
 endQueue = []
@@ -23,11 +28,21 @@ endQueue = []
 
 ##GPIO Pin Setup
 io.setmode(io.BOARD)
-zirc = system(TANKNUM, 1, RACKSPEED, 1)
-zirc.setMotor(IN1, IN2)
-zirc.setSensor(SENSORPIN)
 io.setup(BUTTONPIN, io.IN,pull_up_down=io.PUD_UP)
 ################
+
+def send_command(ser, number):
+    # Assuming param is an integer that needs to be sent as a 2-byte little-endian value
+    number_bytes = number.to_bytes(2, byteorder='little')
+    
+    ser.write(number_bytes)
+    
+def move_to(tankNum):
+    send_command(ser, TANKLOC[tankNum])
+    response = ser.readline().decode().strip()
+    while(response != "DONE"):
+        sleep(0.1)
+        response = ser.readline().decode().strip()
 
 #End rack unloaded button
 def button_callback(channel):
@@ -42,7 +57,7 @@ app = Flask(__name__)
 def add_job_list():
     try:
         job_data = request.json #expecting a list format
-        new_job = job(job_data[0],job_data[1],job_data[2], moveQueue, endQueue, zirc)
+        new_job = job(job_data[0],job_data[1],job_data[2], moveQueue, endQueue, occupiedTanks)
         moveQueue.append(new_job)
         return jsonify({'status': 'success', 'jobID':new_job.jobID}), 200
     except:
@@ -51,6 +66,9 @@ def add_job_list():
 def run_server():
     app.run(host='0.0.0.0', port=5000)
     
+    
+    ser.write(number_bytes)
+    
 server_thread = threading.Thread(target=run_server)
 server_thread.daemon = True
 server_thread.start()
@@ -58,22 +76,20 @@ server_thread.start()
 ##Main loop
 try:
     while True:
-        print(zirc.occupiedTanks)
-        
         ##Check if you can move finished rack to the start
-        if len(endQueue) != 0 and zirc.occupiedTanks[0] != 'X':
+        if len(endQueue) != 0 and occupiedTanks[0] != 'X':
             rackUnloaded = 0
-            zirc.moveTo(ENDRACK)
+            move_to(ENDRACK)
             endQueue.pop(0)
             ##Action to pick up rack##
             sleep(3)
-            zirc.occupiedTanks[ENDRACK] = '0'
+            occupiedTanks[ENDRACK] = '0'
             
-            zirc.moveTo(0)
+            moveTo(STARTRACK)
             
             ##Drop rack into tank##
             sleep(3)
-            zirc.occupiedTanks[STARTRACK] = 'X'
+            occupiedTanks[STARTRACK] = 'X'
             
         
         ##If there are jobs to move
@@ -91,18 +107,18 @@ try:
                 nextUp = moveQueue.pop(i)
                 print('moving job ' + str(nextUp.jobID))
             
-                zirc.moveTo(nextUp.tankNums[nextUp.currentTank])
+                move_to(nextUp.tankNums[nextUp.currentTank])
                 
                 ##Action to pick up rack##
-                zirc.occupiedTanks[nextUp.tankNums[nextUp.currentTank]] = '0'
+                occupiedTanks[nextUp.tankNums[nextUp.currentTank]] = '0'
                 sleep(3)
                 
-                zirc.moveTo(nextUp.next_tank())
+                move_to(nextUp.next_tank())
                 nextUp.currentTank += 1
                 
                 ##Drop rack into tank##
                 sleep(3)
-                zirc.occupiedTanks[nextUp.tankNums[nextUp.currentTank]] = 'X'
+                occupiedTanks[nextUp.tankNums[nextUp.currentTank]] = 'X'
                 
                 nextUp.start_timer(nextUp.tankTimes[nextUp.currentTank])
                 
@@ -114,6 +130,7 @@ except KeyboardInterrupt:
     print('inturrupted by user')
 finally:
     io.cleanup()
+    ser.close()
 
 
 
