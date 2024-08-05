0a3c6143ce74cd7f291f1b73b693ca2b5355e03f
Spencer <95696502+SpencerMiske@users.noreply.github.com>
2024-08-05 13:39:33 +0100

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import sys
 import threading
 from Job import job
 import serial
+from Commands import send_command,move_to,pick_up,lower
 
 from flask import Flask, request, jsonify
 
@@ -28,32 +29,7 @@ endQueue = []
 io.setmode(io.BOARD)
 io.setup(BUTTONPIN, io.IN,pull_up_down=io.PUD_UP)
 ################
-
-def send_command(ser, number):
-    # Assuming param is an integer that needs to be sent as a 2-byte little-endian value
-    number_bytes = number.to_bytes(2, byteorder='little')
     
-    ser.write(number_bytes)
-    
-def move_to(tankLoc):
-    send_command(ser, tankLoc)
-    response = ser.readline().decode().strip()
-    while(response != "DONE"):
-        sleep(0.1)
-        response = ser.readline().decode().strip()
-        
-def pick_up(tankLoc):
-    move_to(tankLoc - BACKUP_DISTANCE)
-    #Lower hoist
-    move_to(tankLoc)
-    #Rasie Hoist
-    
-def lower(tankLoc):
-    #Lower Hoist
-    move_to(tankLoc - BACKUP_DISTANCE)
-    #Raise
-    
-
 #End rack unloaded button
 def button_callback(channel):
     unloaded = 1
@@ -93,7 +69,6 @@ try:
             ##Action to pick up rack##
             pick_up(ANKLOC[ENDRACK])
             
-            sleep(3)
             occupiedTanks[ENDRACK] = '0'
             
             move_to(TANKLOC[STARTRACK])
@@ -101,7 +76,6 @@ try:
             ##Drop rack into tank##
             lower(TANKLOC[STARTRACK])
             
-            sleep(3)
             occupiedTanks[STARTRACK] = 'X'
             
         
@@ -126,7 +100,6 @@ try:
                 pick_up(TANKLOC[nextUp.tankNums[nextUp.currentTank]])
                 
                 occupiedTanks[nextUp.tankNums[nextUp.currentTank]] = '0'
-                sleep(3)
                 
                 move_to(TANKLOC[nextUp.next_tank()])
                 nextUp.currentTank += 1
@@ -134,7 +107,6 @@ try:
                 ##Drop rack into tank##
                 lower(TANKLOC[nextUp.next_tank()])
                 
-                sleep(3)
                 occupiedTanks[nextUp.tankNums[nextUp.currentTank]] = 'X'
                 
                 nextUp.start_timer(nextUp.tankTimes[nextUp.currentTank])
