e0bb17f235e4b23d2bb6e3fd20c19d89c8a9fdb4
Spencer <95696502+SpencerMiske@users.noreply.github.com>
2024-08-06 14:09:36 +0100

    Update Commands.py

--- Commands.py
+++ Commands.py
@@ -1,21 +1,11 @@
 import serial
 
-def add_job_list():
-    try:
-        job_data = request.json #expecting a list format
-        new_job = job(job_data[0],job_data[1],job_data[2], moveQueue, endQueue, occupiedTanks)
-        moveQueue.append(new_job)
-        occupiedTanks[STARTRACK] = 'X'
-        return jsonify({'status': 'success', 'jobID':new_job.jobID}), 200
-    except:
-        print('could not convert input')
-
 def send_command(ser, number):
     # Assuming param is an integer that needs to be sent as a 2-byte little-endian value
     number_bytes = number.to_bytes(2, byteorder='little')
     ser.write(number_bytes)
-    
-def move_to(tankLoc):
+
+def move_to(ser, tankLoc):
     global response_message
     
     send_command(ser, tankLoc)
