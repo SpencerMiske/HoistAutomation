b65cb33091f58401344c4ad39b97cc65e89dba04
Spencer <95696502+SpencerMiske@users.noreply.github.com>
2024-08-23 12:02:10 -0300

    Update ServerSending.py

--- ServerSending.py
+++ ServerSending.py
@@ -3,7 +3,7 @@ from tkinter import messagebox
 import requests
 
 # Function to send job data to the Flask server
-def send_job(job_id, tank_times, tank_nums):
+def send_job(job_id, tank_times, tank_nums, speed):
     job_data = [job_id, tank_times, tank_nums]
     try:
         response = requests.post('http://192.168.15.145:5000/add_job_list', json=job_data)
@@ -28,6 +28,11 @@ tank_times_entry = tk.Entry(root)
 tank_times_entry.pack(pady=5)
 
 # Tank Numbers Entry
+tk.Label(root, text="Hoist Speed").pack(pady=5)
+hoist_speed_entry = tk.Entry(root)
+hoist_speed_entry.pack(pady=5)
+
+# speed Entry
 tk.Label(root, text="Tank Numbers (comma-separated)").pack(pady=5)
 tank_nums_entry = tk.Entry(root)
 tank_nums_entry.pack(pady=5)
@@ -42,6 +47,8 @@ zone_count_menu.pack(pady=5)
 # Send Job Button
 def send_job_button_click():
     job_id = int(job_id_entry.get())
+
+    speed = int(hoist_speed_entry.get())
     
     # Parse tank times and insert "START" and "END"
     tank_times = list(map(int, tank_times_entry.get().split(',')))
@@ -56,7 +63,7 @@ def send_job_button_click():
     num_zones = int(zone_count_var.get())
     tank_nums.append(num_zones - 1)  # Add last zone number
 
-    send_job(job_id, tank_times, tank_nums)
+    send_job(job_id, tank_times, tank_nums, speed)
 
 send_button = tk.Button(root, text="Send Job", command=send_job_button_click)
 send_button.pack(pady=20)
